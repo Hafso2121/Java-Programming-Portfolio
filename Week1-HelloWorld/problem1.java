38d62de0600d5a9160fc05002f9b2e35edde01aa
qaalidumar <142459120+qaalidumar@users.noreply.github.com>
2026-05-05 19:06:48 -0400

    Week 1: HelloWorld & problem1

---
+++ Week1-HelloWorld/problem1.java
@@ -0,0 +1,12 @@
+public class problem1{
+public static void main(String[] args){
+System.out.println("My name is Hafsa and i'm in my 20th");
+System.out.println("I'm good at coding ");
+System.out.println("The Sum of 10 + 15 =  " + (10 + 15));
+System.out.println("Floating Point of 22.35");  
+System.out.println('H');
+System.out.println(" * ");
+System.out.println(" ** ");
+System.out.println(" *** ");
+ }
+}
